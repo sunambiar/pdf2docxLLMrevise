5d8a816813a888182cb1d5b0834f45aaf7882e65
Nambiar <nambiarsu@gmail.com>
2026-01-30 23:09:35 +0530

    new file:   LanguageTool-stable/Dockerfile 	new file:   LanguageTool-stable/snRunGrammar.sh

---
+++ LanguageTool-stable/snRunGrammar.sh
@@ -0,0 +1,13 @@
+
+
+ 
+
+git config --global http.proxy "http://ioc%5c00023569:321%402026Jan@mdproxy.ds.indianoil.in:8080"
+git config --global https.proxy "http://ioc%5c00023569:321%402026Jan@mdproxy.ds.indianoil.in:8080"
+
+
+docker build -t grammar . 
+
+
+
+
